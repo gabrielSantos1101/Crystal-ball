3352145352cbc4d421095543e2f8feb7f332f3fe
Gabriel Santos <105078208+Gabriel-Santos01@users.noreply.github.com>
2022-06-10 11:56:34 -0300

    Códigos V1

---
+++ app.js
@@ -0,0 +1,49 @@
+const respostaElement = document.querySelector('#resposta')
+const inputPergunta = document.querySelector('#pergunta')
+const questButton = document.querySelector('#questButton')
+const respostas = [
+  'Certeza!',
+  'Não tenho certeza.',
+  'Não conte com isso.',
+  'É decididamente assim.',
+  'Sem duvidas.',
+  'Pergunte novamente mas tarde.',
+  'Sim, definitivamente!',
+  'Minha resposta é não.',
+  'Você pode contar com isso.',
+  'Melho não te dizer agora.',
+  'a meu ver, sim.',
+  'Minhas fontes dizem que não.',
+  'provavelmente.',
+  'Não é possivel prever agora.',
+  'Perspectiva boa.',
+  'sim.',
+  'as perspectivas não são tão boas.',
+  'Concentre-se e pergunte novamente.',
+  'Sinais apontam que sim'
+]
+
+//Fazer pergunta ao clicar
+function fazerPergunta() {
+  if (inputPergunta.value == '') {
+    alert('Por favor digite uma pergunta')
+    return
+  }
+
+  questButton.setAttribute('disabled', true)
+
+  const pergunta = '<div>' + inputPergunta.value + '</div>'
+
+  const totalRespostas = respostas.length
+  const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+  respostaElement.innerHTML = pergunta + respostas[numeroAleatorio]
+
+  respostaElement.innerHTML = pergunta + respostas[numeroAleatorio]
+  respostaElement.style.opacity = 1
+
+  //sumir resposta
+  setTimeout(function () {
+    respostaElement.style.opacity = 0
+    questButton.removeAttribute('disabled')
+  }, 3000)
+}
